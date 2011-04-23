d59ad9585b131eecffd37dbd0b2de30c890e0493
Peter Suschlik <peter@suschlik.de>
2011-04-23 13:06:52 +0200

    Add alternative icons

--- lib/riot_notifier/libnotify.rb
+++ lib/riot_notifier/libnotify.rb
@@ -2,15 +2,26 @@
 module RiotNotifier
 
   class Libnotify < Base
+    ICON = {
+      :green => %w[
+        /usr/share/icons/gnome/scalable/emblems/emblem-default.svg
+        /usr/share/icons/gnome/256x256/emotes/face-laugh.png
+      ],
+      :red => %w[
+        /usr/share/icons/gnome/scalable/emotes/face-angry.svg
+        /usr/share/icons/gnome/256x256/emotes/face-angry.png
+      ]
+    }
+
     OPTIONS = {
       :green => {
-        :icon_path => "/usr/share/icons/gnome/scalable/emblems/emblem-default.svg",
+        :icon_path => ICON[:green].find { |path| File.exist?(path) },
         :timeout => 2.5,
         :urgency => :normal,
         :summary => ":-)"
       },
       :red => {
-        :icon_path => "/usr/share/icons/gnome/scalable/emotes/face-angry.svg",
+        :icon_path => ICON[:red].find { |path| File.exist?(path) },
         :timeout => 2.5,
         :urgency => :critical,
         :summary => ":-("
